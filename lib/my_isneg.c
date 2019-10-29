488933090979d901594ff07c3cc1d914cd1861e0
Sheldon Rupp <sheldon.rupp@epitech.eu>
2019-10-29 23:13:01 +0100

    Add my_isneg.c

---
+++ lib/my_isneg.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2019
+** my_isneg
+** File description:
+**
+*/
+
+int my_isneg(int n)
+{
+    int x = 0;
+
+    if (n < x) {
+        my_putchar('N');
+        my_putchar('\n');
+    } else {
+        my_putchar('P');
+        my_putchar('\n');
+    }
+}
