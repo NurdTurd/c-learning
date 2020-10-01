a0817fc2a90266bb92c32d882a2a9c398ac5edf3
Anthony Luong <luong.anthony@epitech.eu>
2020-10-01 20:13:24 +0200

    add my params to array function

---
+++ lib/my_params_to_array.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2019
+** my_params_to_array
+** File description:
+** 
+*/
+
+struct info_param *my_params_to_array(int ac, char **av)
+{
+        int length;
+        char *str;
+        char *copy;
+        char **word_array;
+}
