e571118905abceb193b7e571fcf51b6c60bafad7
Restyled.io <commits@restyled.io>
2020-10-01 17:47:45 +0000

    Restyled by clang-format

--- examples/print-ascii-char-value.c
+++ examples/print-ascii-char-value.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main() {
-    char c;
-    printf("Enter a character: ");
-    scanf("%c", &c);
+  char c;
+  printf("Enter a character: ");
+  scanf("%c", &c);
 
-    // %d displays the integer value of a character
-    // %c displays the actual character
-    printf("ASCII value of %c = %d", c, c);
+  // %d displays the integer value of a character
+  // %c displays the actual character
+  printf("ASCII value of %c = %d", c, c);
 
-    return 0;
+  return 0;
 }
