742c471b221413dbbd0ce89e93b169f7a8213ccf
Restyled.io <commits@restyled.io>
2020-10-01 18:13:47 +0000

    Restyled by astyle

--- lib/my_params_to_array.c
+++ lib/my_params_to_array.c
@@ -2,13 +2,13 @@
 ** EPITECH PROJECT, 2019
 ** my_params_to_array
 ** File description:
-** 
+**
 */
 
 struct info_param *my_params_to_array(int ac, char **av)
 {
-        int length;
-        char *str;
-        char *copy;
-        char **word_array;
+    int length;
+    char *str;
+    char *copy;
+    char **word_array;
 }
