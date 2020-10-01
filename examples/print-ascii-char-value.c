c10923295c61481ed8307db0193b9e1957c579d1
Restyled.io <commits@restyled.io>
2020-10-01 17:47:42 +0000

    Restyled by astyle

--- examples/print-ascii-char-value.c
+++ examples/print-ascii-char-value.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-int main() {  
+int main() {
     char c;
     printf("Enter a character: ");
-    scanf("%c", &c);  
-    
+    scanf("%c", &c);
+
     // %d displays the integer value of a character
     // %c displays the actual character
     printf("ASCII value of %c = %d", c, c);
-    
+
     return 0;
 }
