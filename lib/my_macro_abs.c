90b46e0ff683be1c77de4d8c327d97e61344fb41
Anthony Luong <luong.anthony@epitech.eu>
2020-10-01 20:01:12 +0200

    Add my_macro_abs

---
+++ lib/my_macro_abs.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2019
+** my_macro_abs.h
+** File description:
+** 
+*/
+
+#include <stdio.h>
+#include "include/my_macro_abs.h"
+
+int main(void)
+{
+        printf("%d", ABS(-10));
+}
